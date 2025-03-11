6a42bafd419370b969cb7ac667b8eb43ad212094
Pablo Astete Elguera <github@astete.cloud>
2025-03-11 11:06:28 -0500

    vug commit

--- Modulos/botteste.sh
+++ Modulos/botteste.sh
@@ -10,7 +10,7 @@
     echo -e "\033[1;32mINICIANDO BOT TESTE \033[0m\n"
     cd $HOME/BOT
     rm -rf $HOME/BOT/botssh
-    wget https://github.com/MEGALODON-SSH/MEGA/raw/main/Sistema/botssh >/dev/null 2>&1
+    wget https://github.com//dev-error404/panel-vpn/raw/main/Sistema/botssh >/dev/null 2>&1
     chmod 777 botssh
     echo ""
     sleep 1
